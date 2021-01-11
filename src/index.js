68b220da348175f32066f8830fa80b394e588468
yasmin <yasminsheffer21@gmail.com>
2021-01-11 20:46:15 +0200

    installing ans set parse

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Parse from 'parse';
+
+Parse.serverURL = 'https://parseapi.back4app.com'; // This is your Server URL
+Parse.initialize(
+  '51WTBPdplru4mxpDvag4nrEUfNZgUyfxbIgCy7ax', // This is your Application ID
+  'OBjrtMi5wsuIwzeW09gsZoP9FFclzAoGUU9bhTaW' // This is your Javascript key
+);
 
 ReactDOM.render(
   <React.StrictMode>
